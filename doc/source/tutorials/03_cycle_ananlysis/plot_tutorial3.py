d704e04aa9b73ec5dfb92f2313b9a04da649e62e
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2020-08-10 17:53:20 +0100

    Finish first pass at cycle waveform shape tutorial

--- doc/source/tutorials/03_cycle_ananlysis/plot_tutorial3.py
+++ doc/source/tutorials/03_cycle_ananlysis/plot_tutorial3.py
@@ -13,7 +13,7 @@ alignment
 
 import emd
 import numpy as np
-from scipy import signal
+from scipy import signal, stats
 import matplotlib.pyplot as plt
 
 #%%
@@ -65,9 +65,9 @@ x = np.cos(2*np.pi*f*t)
 #%%
 # We then modulate our signal ``x`` by the linear and nonlinear systems.
 
-K = .5
-power = 2
+K = .5  # both systems will scale the signal by 0.5
 
+# Apply the systems to time-series x
 x_linear = linear_system(x, K)
 x_nonlinear = nonlinear_system(x, K)
 
@@ -171,7 +171,7 @@ plt.xlabel('Frequency (Hz)')
 # Hilbert-Huang spectra alongside the instantaneous frequency traces for the
 # linear and non-linear systems.
 
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(10, 6))
 plt.axes([.1, .1, .2, .8])
 plt.plot(spec_linear[:, 0], centres)
 plt.plot(spec_nonlinear[:, 0], centres)
@@ -215,11 +215,15 @@ plt.subplot(211)
 plt.plot(t, x_linear)
 plt.plot(t, x_nonlinear)
 plt.xlim(0, 3)
+plt.ylabel('Signal\nAmplitude')
 plt.subplot(212)
 plt.plot(t, IF_linear[:, 0])
 plt.plot(t, IF_nonlinear[:, 0])
 plt.xlim(0, 3)
 plt.ylim(0, 4)
+plt.xlabel('Time (seconds)')
+plt.ylabel('Instantaneous\nFrequency (Hz)')
+plt.legend(['Linear system', 'Nonlinear-system'])
 
 #%%
 # The peaks in instantaneous frequency co-incide with the peaks of the raw
@@ -232,6 +236,131 @@ plt.ylim(0, 4)
 # trough respectively. The instantnaous frequency tracks this at the full
 # sample rate of the data showing high frequnecies around the sharp peaks and
 # low frequencies around the slow troughs.
+
+#%%
+# Single cycle Instantaneous Frequency
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+#%%
+# We can extract the instantaneous frequency of single cycles of an oscillation
+# to systematically compare differences in waveform shape between signals. Next
+# we extract the indices of individual cycles in our systems and extract the
+# IMF time-course waveform and instantaneous frequency of these cycles into
+# separate matrices.
+
+# Find cycle indices
+cycles_linear = emd.cycles.get_cycle_inds(IP_linear, return_good=True)
+cycles_nonlinear = emd.cycles.get_cycle_inds(IP_nonlinear, return_good=True)
+
+# Pre-allocate an array for linear cycles
+waveform_linear = np.zeros((300, cycles_linear.max()))*np.nan
+instfreq_linear = np.zeros((300, cycles_linear.max()))*np.nan
+
+# Extract waveform and IF info
+for ii in range(1, cycles_linear.max()+1):
+    inds = cycles_linear[:, 0] == ii
+    waveform_linear[:np.sum(inds), ii-1] = imf_linear[inds, 0]
+    instfreq_linear[:np.sum(inds), ii-1] = IF_linear[inds, 0]
+
+# Pre-allocate an array for non-linear cycles
+waveform_nonlinear = np.zeros((300, cycles_nonlinear.max()))*np.nan
+instfreq_nonlinear = np.zeros((300, cycles_nonlinear.max()))*np.nan
+
+# Extract waveform and IF info
+for ii in range(1, cycles_nonlinear.max()+1):
+    inds = cycles_nonlinear[:, 0] == ii
+    waveform_nonlinear[:np.sum(inds), ii-1] = imf_nonlinear[inds, 0]
+    instfreq_nonlinear[:np.sum(inds), ii-1] = IF_nonlinear[inds, 0]
+
+
+#%%
+# We next directly compare waveform of examples cycles from the linear and
+# non-linear systems. We plot the fifth cycle as an example. The first panel
+# plots the waveform of the example cycles, there is a clear difference in
+# waveform shape between the linear and non-linear systems. Panel 2 plots the
+# instantaneous phase, the phase of the linear system progresses in a straight
+# line whilst the nonlinear phase progresses at different rates throughout the
+# cycle - it starts quickly but then slows. These dynamics are illustrated in
+# panel 3 showing the instantaneous frequencies (computed from the derivative
+# of the phase). The non-linear cycle has a relatively high frequency at the
+# start of the cycle and a relatively low frequency at the end. In contrast,
+# the linear cycle has a constant frequency.
+
+cycle_ind_linear = cycles_linear[:, 0] == 5
+cycle_ind_nonlinear = cycles_nonlinear[:, 0] == 5
+
+plt.figure(figsize=(4, 8))
+plt.subplots_adjust(left=.25, hspace=.3)
+plt.subplot(311)
+plt.plot(imf_linear[cycle_ind_linear, 0])
+plt.plot(imf_nonlinear[cycle_ind_nonlinear, 0])
+plt.xlabel('Samples')
+plt.grid(True)
+
+plt.subplot(312)
+plt.plot(IP_linear[cycle_ind_linear, 0])
+plt.plot(IP_nonlinear[cycle_ind_nonlinear, 0])
+plt.ylabel('Instataneous\nPhase')
+plt.xlabel('Samples')
+plt.yticks(np.linspace(0, np.pi*2, 5), [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$'])
+plt.grid(True)
+
+plt.subplot(313)
+plt.plot(IF_linear[cycle_ind_linear, 0])
+plt.plot(IF_nonlinear[cycle_ind_nonlinear, 0])
+plt.ylabel('Instataneous\nFrequency')
+plt.xlabel('Samples')
+plt.grid(True)
+
+#%%
+# Unfortuanetely, We cannot directly compare these two instantaneous frequency
+# profiles as they are. Though both are the same length the change in waveform
+# shape means that the x-axis is misaligned between them. Specifically, we can
+# see that the nonlinear cycle has a descending zero-crossing around 100
+# samples into the cycle whilst the linear cycle crosses zero around 125
+# samples in. Similarly the peak and trough are a little shifted between the
+# cycles.
+#
+# As such, if we simply compute a comparison between these cycles, we cannot be
+# sure we're comparing featuers like for like. We may be contrasting the
+# shoulder of one cycle with the zero-crossing of another.
+#
+# One solution is called phase-alignment. We can plot the evolution of each
+# cycle as over its phase rather than over time. We do this for the waveform
+# and the instantaneous frequency next.
+
+plt.figure(figsize=(10, 4))
+plt.subplots_adjust(bottom=.15, wspace=.3)
+plt.subplot(121)
+plt.plot(IP_linear[cycle_ind_linear, 0], imf_linear[cycle_ind_linear, 0])
+plt.plot(IP_nonlinear[cycle_ind_nonlinear, 0], imf_nonlinear[cycle_ind_nonlinear, 0])
+plt.xlabel('Instataneous\nPhase')
+plt.ylabel('Instataneous\nFrequency')
+plt.xticks(np.linspace(0, np.pi*2, 5), [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$'])
+plt.grid(True)
+plt.title('Phase-aligned\nWaveforms')
+
+plt.subplot(122)
+plt.plot(IP_linear[cycle_ind_linear, 0], IF_linear[cycle_ind_linear, 0])
+plt.plot(IP_nonlinear[cycle_ind_nonlinear, 0], IF_nonlinear[cycle_ind_nonlinear, 0])
+plt.xlabel('Instataneous\nPhase')
+plt.ylabel('Instataneous\nFrequency')
+plt.xticks(np.linspace(0, np.pi*2, 5), [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$'])
+plt.grid(True)
+plt.title('Phase-aligned\nInst. Freq')
+
+#%%
+# The phase-alignment makes the waveform of both cycles nearly exactly
+# sinusoidal. Crucially information about the waveform shape of the cycles in
+# the instantaneous frequency is preserved. We can see that at the cycle peak
+# (where phase is pi/2) the nonlinear cyclehas a much higher frequency than the
+# linear cycle. The frequencies of the two cycles are nearly equal just after
+# the ascending zerp-crossing (where phase is pi) and the nonlinear cycle has a
+# much lower frequency around the trough.
+#
+# This presentation of the data makes comparisons across cycles much simpler.
+# We can now discuss differences between the shape of specific parts of a cycle
+# - such as the peak or trough.
 
 #%%
 # A dynamic oscillation with noise
@@ -306,10 +435,13 @@ plt.subplot(121)
 plt.plot(f, pxx_linear)
 plt.plot(f, pxx_nonlinear)
 plt.title("Welch's Periodogram")
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Power')
 plt.xlim(0, 40)
 plt.subplot(122)
 plt.plot(centres, spec_linear[:, 2])
 plt.plot(centres, spec_nonlinear[:, 2])
+plt.xlabel('Frequency (Hz)')
 plt.title("Hilbert-Huang Transform")
 plt.legend(['Linear System', 'Nonlinear System'])
 
@@ -322,7 +454,7 @@ plt.legend(['Linear System', 'Nonlinear System'])
 # We can plot the waveform and instantanous frequency alongside each other to
 # try and see how the shape might be affecting instantanous frequency.
 
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(8, 6))
 plt.subplot(211)
 plt.plot(t, imf_linear[:, 2])
 plt.plot(t, imf_nonlinear[:, 2])
@@ -353,32 +485,256 @@ cycles_linear = emd.cycles.get_cycle_inds(IP_linear, return_good=True, mask=IA_l
 cycles_nonlinear = emd.cycles.get_cycle_inds(IP_nonlinear, return_good=True, mask=IA_nonlinear[:, 2] > .05)
 
 #%%
-# We can now plot just the 'good' cycles in the analysis locked to the ascending zero-crossing.
+# Next we extract the per-cycle IMF and instantaneous frequencies for both
+# systems. We will also compute the 'control points' for each cycle, these are
+# the indices of the peak, zero-crossings and trough of each cycle.
 
-plt.figure()
-ax1 = plt.subplot(121)
-ax2 = plt.subplot(122)
+waveform_linear = np.zeros((60, cycles_linear.max()))*np.nan
+instfreq_linear = np.zeros((60, cycles_linear.max()))*np.nan
+
 for ii in range(1, cycles_linear.max()+1):
     inds = cycles_linear[:, 2] == ii
-    ax1.plot(imf_linear[inds, 2])
-    ax2.plot(IF_linear[inds, 2])
+    waveform_linear[:np.sum(inds), ii-1] = imf_linear[inds, 2]
+    instfreq_linear[:np.sum(inds), ii-1] = IF_linear[inds, 2]
 
-plt.figure()
-ax1 = plt.subplot(121)
-ax2 = plt.subplot(122)
+ctrl_linear = emd.cycles.get_control_points(imf_linear[:, 2], cycles_linear[:, 2])
+
+waveform_nonlinear = np.zeros((60, cycles_nonlinear.max()))*np.nan
+instfreq_nonlinear = np.zeros((60, cycles_nonlinear.max()))*np.nan
+
 for ii in range(1, cycles_nonlinear.max()+1):
     inds = cycles_nonlinear[:, 2] == ii
-    ax1.plot(imf_nonlinear[inds, 2])
-    ax2.plot(IF_nonlinear[inds, 2])
+    waveform_nonlinear[:np.sum(inds), ii-1] = imf_nonlinear[inds, 2]
+    instfreq_nonlinear[:np.sum(inds), ii-1] = IF_nonlinear[inds, 2]
+
+ctrl_nonlinear = emd.cycles.get_control_points(imf_nonlinear[:, 2], cycles_nonlinear[:, 2])
+
+#%%
+# Next we plot a big summary of the single cycle IMFs. The top row contains
+# each single cycle and the second row contains the average across cycles. As
+# with the simplex oscillation in the first example, we can see by eye that the
+# variability in the signal is shifting the relative timing of the peaks and
+# troughs within each cycle.
+#
+# Unfortunately there is a second problem caused by the dynamics in this
+# signal. As well as shape induced variability in the timinig of features
+# within cycles, there is also variability in the overall durations of each
+# cycle. These both add up to create considerable noise in cross cycle comparisons.
+#
+# The third row contains histograms of the timings of the control points within
+# each cycle. The further we get from the 'locking-point' at the start of the
+# cycle, the wider these distributions become. The troughs can occur anywhere
+# between 25 and 40 samples into a cycle.
+#
+# Finally the fourth row show the number of cycles contributing to the average
+# at each point in time. After 40 samples there are relatively few cycles long
+# enough to contribute to an average at this point. Around 50-60 cycles we have
+# almost none.
+
+
+plt.figure(figsize=(6, 9))
+plt.subplot(421)
+plt.title('Linear oscillations')
+plt.plot(waveform_linear)
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(422)
+plt.title('Nonlinear oscillations')
+plt.plot(waveform_nonlinear)
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(423)
+plt.title('Linear avg. waveform')
+plt.plot(np.nanmean(waveform_linear, axis=1))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(424)
+plt.title('Nonlinear avg. waveform')
+plt.plot(np.nanmean(waveform_nonlinear, axis=1))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(425)
+plt.title('Linear ctrl points')
+plt.hist(ctrl_linear[:, 1], np.linspace(0, 50))
+plt.hist(ctrl_linear[:, 2], np.linspace(0, 50))
+plt.hist(ctrl_linear[:, 3], np.linspace(0, 50))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(426)
+plt.title('Nonlinear ctrl points')
+plt.hist(ctrl_nonlinear[:, 1], np.linspace(0, 50))
+plt.hist(ctrl_nonlinear[:, 2], np.linspace(0, 50))
+plt.hist(ctrl_nonlinear[:, 3], np.linspace(0, 50))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+plt.legend(['Peak', 'Desc ZC', 'Trough'])
+
+plt.subplot(427)
+plt.title('Linear Num Cycles')
+plt.plot(np.sum(~np.isnan(waveform_linear), axis=1))
+plt.xticks(np.arange(7)*10)
+plt.grid(True)
+
+plt.subplot(428)
+plt.xlabel('Samples')
+plt.title('Nonlinear Num Cycles')
+plt.plot(np.sum(~np.isnan(waveform_nonlinear), axis=1))
+plt.xticks(np.arange(7)*10)
+plt.grid(True)
 
 
 #%%
+# We can make the equivalent plot for the instantaneous frequency - again the
+# two sources in variability are visible.
 
-pa_linear = emd.cycles.phase_align(IP_linear[:, 2, None], IF_linear[:, 2], cycles=cycles_linear[:, 2, None])
-pa_nonlinear = emd.cycles.phase_align(IP_nonlinear[:, 2, None], IF_nonlinear[:, 2], cycles=cycles_nonlinear[:, 2, None])
+plt.figure(figsize=(6, 9))
+plt.subplot(421)
+plt.title('Linear oscillations')
+plt.plot(instfreq_linear)
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
 
-plt.figure()
-plt.plot(pa_linear.mean(axis=1))
-plt.plot(pa_nonlinear.mean(axis=1))
+plt.subplot(422)
+plt.title('Nonlinear oscillations')
+plt.plot(instfreq_nonlinear)
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(423)
+plt.title('Linear avg. instfreq')
+plt.plot(np.nanmean(instfreq_linear, axis=1))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(424)
+plt.title('Nonlinear avg. instfreq')
+plt.plot(np.nanmean(instfreq_nonlinear, axis=1))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(425)
+plt.title('Linear ctrl points')
+plt.hist(ctrl_linear[:, 1], np.linspace(0, 50))
+plt.hist(ctrl_linear[:, 2], np.linspace(0, 50))
+plt.hist(ctrl_linear[:, 3], np.linspace(0, 50))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+
+plt.subplot(426)
+plt.title('Nonlinear ctrl points')
+plt.hist(ctrl_nonlinear[:, 1], np.linspace(0, 50))
+plt.hist(ctrl_nonlinear[:, 2], np.linspace(0, 50))
+plt.hist(ctrl_nonlinear[:, 3], np.linspace(0, 50))
+plt.xticks(np.arange(7)*10, [])
+plt.grid(True)
+plt.legend(['Peak', 'Desc ZC', 'Trough'])
+
+plt.subplot(427)
+plt.title('Linear Num Cycles')
+plt.plot(np.sum(~np.isnan(instfreq_linear), axis=1))
+plt.xticks(np.arange(7)*10)
+plt.grid(True)
+
+plt.subplot(428)
+plt.xlabel('Samples')
+plt.title('Nonlinear Num Cycles')
+plt.plot(np.sum(~np.isnan(instfreq_nonlinear), axis=1))
+plt.xticks(np.arange(7)*10)
+plt.grid(True)
+
+#%%
+# We can see an indication that there may be a difference in shape between the
+# linear and nonlinear systems but it is hard to interpret when looking at the
+# waveform or instantaneous frequency as a function of time.
+#
+# As in the first section, we can make a cleaner comparison considering the
+# evolution of a cycle as a function of its phase rather than across time. Next
+# we compute this phase alignment using ``emd.cycles.phase_align``. This
+# interpolates the instantaneous phase of a signal onto a regularly sampled
+# grid before applying that interpolation onto a metric of our choice. Here we
+# will phase align the IMF and the instantaneous frequency values for the
+# linear and non linear systems.
+
+pa_waveform_linear = emd.cycles.phase_align(IP_linear[:, 2, None],
+                                            imf_linear[:, 2],
+                                            cycles=cycles_linear[:, 2, None])
+pa_waveform_nonlinear = emd.cycles.phase_align(IP_nonlinear[:, 2, None],
+                                               imf_nonlinear[:, 2],
+                                               cycles=cycles_nonlinear[:, 2, None])
+
+pa_if_linear = emd.cycles.phase_align(IP_linear[:, 2, None],
+                                      IF_linear[:, 2],
+                                      cycles=cycles_linear[:, 2, None])
+pa_if_nonlinear = emd.cycles.phase_align(IP_nonlinear[:, 2, None],
+                                         IF_nonlinear[:, 2],
+                                         cycles=cycles_nonlinear[:, 2, None])
+
+phase_template = np.linspace(0, np.pi*2, 48)
+
+#%%
+# Plotting the phase aligned results, we see that the IMF waveforms are
+# transformed into near perfect sinusoids confirming that the phase alignment
+# has done a good job at projecting the phase onto a regular grid.
+#
+# When applying this projection onto the instantaneous frequencies we can see
+# that the nonlinear cycle has a higher frequency peak and lower frequency
+# trough than the linear cycles. Thanks to the phase alignment we can be
+# confident that we are really comparing features like-for-like between the two
+# system despite variability in timing of features within cycles and
+# variability in the absolution duration of cycles.
+
+
+plt.figure(figsize=(10, 4))
+plt.subplots_adjust(bottom=.15, wspace=.3)
+plt.subplot(121)
+plt.plot(phase_template, pa_waveform_linear.mean(axis=1))
+plt.plot(phase_template, pa_waveform_nonlinear.mean(axis=1))
+plt.xlabel('Instataneous\nPhase')
+plt.xticks(np.linspace(0, np.pi*2, 5), [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$'])
+plt.grid(True)
+plt.title('Phase-aligned\nIMF')
+
+plt.subplot(122)
+plt.plot(phase_template, pa_if_linear.mean(axis=1))
+plt.plot(phase_template, pa_if_nonlinear.mean(axis=1))
 plt.ylim(10, 16)
 plt.legend(['Linear System', 'Nonlinear system'])
+plt.xlabel('Instataneous\nPhase')
+plt.ylabel('Instataneous\nFrequency')
+plt.xticks(np.linspace(0, np.pi*2, 5), [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$'])
+plt.grid(True)
+plt.title('Phase-aligned\nInst. Freq')
+
+
+#%%
+# We can now use conventional statistics to compare waveforms between groups.
+# Here we do an independent samples t-test to describe differences in phase
+# aligned instantaneous frequency between the linear and nonlinear systems.
+
+tvals, pvals = stats.ttest_ind(pa_if_nonlinear, pa_if_linear, axis=1)
+
+plt.figure(figsize=(10, 4))
+plt.subplots_adjust(bottom=.15, wspace=.3)
+plt.subplot(121)
+plt.plot(phase_template, pa_if_linear.mean(axis=1))
+plt.plot(phase_template, pa_if_nonlinear.mean(axis=1))
+plt.ylim(10, 16)
+plt.legend(['Linear System', 'Nonlinear system'])
+plt.xlabel('Instataneous\nPhase')
+plt.ylabel('Instataneous\nFrequency')
+plt.xticks(np.linspace(0, np.pi*2, 5), [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$'])
+plt.grid(True)
+plt.title('Phase-aligned\nInst. Freq')
+
+plt.subplot(122)
+plt.plot(phase_template, tvals)
+plt.xlabel('Instataneous\nPhase')
+plt.ylabel('t-statistic')
+plt.xticks(np.linspace(0, np.pi*2, 5), [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$'])
+plt.grid(True)
+plt.title('Nonlinear > Linear IF')
